f7c37d675f5af734a0437d67edb7f459a26cbcd7
Artemas Radik <ajradik@gmail.com>
2023-02-09 02:27:19 -0500

    progress

--- client.py
+++ client.py
@@ -1,37 +1,76 @@
-# Python program to implement client side of chat room.
 import socket
 import select
+import struct
 import sys
+from enum import Enum
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class Payload(Enum):
+    reg = 0
+    log = 1
+    dlt = 2
+    acd = 3
+    acf = 4
+    msg = 5
+    err = 6
+    suc = 7
+    nms = 8
  
+def decode(buffer):
+    code, payload = struct.unpack('B528s', buffer)
+    match code:
+        case Payload.err.value:
+            message = struct.unpack('256s272x', payload)
+            return f"{bcolors.WARNING}[WARNING]: {message[0].decode('ascii')}{bcolors.ENDC}"
+        case Payload.suc.value:
+            message = struct.unpack('256s272x', payload)
+            return f"{bcolors.OKGREEN}[SUCCESS]: {message[0].decode('ascii')}{bcolors.ENDC}"
+        case Payload.nms.value:
+            from_username, content = struct.unpack('16s512s', payload)
+            return f"[{from_username.decode('ascii')}] {content.decode('ascii')}"
+        case _:
+            sys.exit(f"{bcolors.FAIL}[FAILURE]: Undecodable transfer buffer received.{bcolors.ENDC}")
+
+def encode(command):
+    elements = command.split(' ')
+    # TODO: Check fields to make sure they fit in types.
+    match elements[0]:
+        case Payload.reg.name:
+            pass
+        case Payload.log.name:
+            pass
+        case Payload.dlt.name:
+            pass
+        case Payload.acd.name:
+            pass
+        case Payload.acf.name:
+            pass
+        case Payload.msg.name:
+            pass
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
-    print ("Correct usage: script, IP address, port number")
-    exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
-server.connect((IP_address, Port))
+    sys.exit(f"{bcolors.FAIL}[FAILURE]: CORRECT USAGE: {bcolors.ENDC}{bcolors.BOLD}python3 client.py [IP] [PORT]{bcolors.ENDC}")
+ip = str(sys.argv[1])
+port = int(sys.argv[2])
+server.connect((ip, port))
 
-server_running = True
-while server_running:
- 
-    # maintains a list of possible input streams
+while True:
     sockets_list = [sys.stdin, server]
- 
-    """ There are two possible input situations. Either the
-    user wants to give manual input to send to other people,
-    or the server is sending a message to be printed on the
-    screen. Select returns from sockets_list, the stream that
-    is reader for input. So for example, if the server wants
-    to send a message, then the if condition will hold true
-    below.If the user wants to send a message, the else
-    condition will evaluate as true"""
     read_sockets, write_socket, error_socket = select.select(sockets_list,[],[])
 
     for socks in read_sockets:
         if socks == server:
-            message = socks.recv(2048).decode('utf-8')
-            print(message.strip())
+            message = print(decode(socks.recv(529)))
         else:
-            message = sys.stdin.readline()
-            server.send(message.encode('utf-8'))
-server.close()
+            server.send(sys.stdin.readline().strip().encode('ascii'))
