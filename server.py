f9767d189b39c6b9dfd32e6b726efae9d710f85e
Artemas Radik <ajradik@gmail.com>
2023-04-10 10:56:39 -0400

    update to be more robust for deployment

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import select
 import sys
+import os
 from _thread import *
 import threading
 import csv, pickle
@@ -155,15 +156,10 @@ def clientthread(conn):
 if __name__ == "__main__":
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	#try: 
 	IP_address = str(sys.argv[1])
 	Port = int(sys.argv[2])
-	"""except:
-		IP_address = "52.152.216.212"
-		Port = 5000"""
 	
-	base = "/Users/swatigoel/Dropbox/college/cs262/cs262/"
-	pending_file = base + str(sys.argv[3])
+	pending_file = os.getcwd() + str(sys.argv[3])
 	server.bind((IP_address, Port))
 	server.listen(100)
 
